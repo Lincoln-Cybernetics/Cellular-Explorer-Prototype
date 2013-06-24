0366432ece30169107c2aea082bffa1ba6db17ad
Matt Ahlschwede <perscrumption@hotmail.com>
2013-06-23 23:14:50 -0500

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -14,9 +14,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 class Cell{
-  	boolean[][] neighborhood = new boolean[3][3];
+		boolean[][] neighborhood = new boolean[3][3];
 		int maturity = 1;
-		
+		public Cell(int a){}
 		public Cell(){}
 		public boolean iterate(boolean neighborhood[][]){return false;}
 	}	
@@ -24,6 +24,7 @@ class Cell{
 class blinkCell extends Cell{
 	boolean blinkstate = false;
 	public blinkCell(){}
+	public blinkCell(int a){}
 	public boolean iterate(boolean neighborhood[][]){ 
 		if (blinkstate == false){ blinkstate = true; return blinkstate;}
 		else{blinkstate = false; return blinkstate;}
@@ -32,11 +33,13 @@ class blinkCell extends Cell{
 
 class onCell extends Cell{
 	public onCell(){}
+	public onCell(int a){}
 	public boolean iterate(boolean neighborhood[][]){return true;}
 }
 
 class blinkCell2 extends Cell{
 	boolean blinkstate = true;
+	public blinkCell2(int a){}
 	public blinkCell2(){}
 	public boolean iterate(boolean neighborhood[][]){ 
 		if (blinkstate == false){ blinkstate = true; return blinkstate;}
@@ -52,8 +55,8 @@ class Conway extends Cell{
 	int b;
 	boolean active;
 	public Conway(){}
-	public Conway(int c){
-		maturity = c;}
+	public Conway(int a){
+		maturity = a;}
 		
 	public boolean iterate(boolean neighborhood[][]){
 		counter += 1;
@@ -72,6 +75,51 @@ class Conway extends Cell{
 					case 2: active =  neighborhood[1][1];
 					break;
 					case 3: active = true;
+					break;
+					case 4: active = false;
+					break;
+					case 5: active = false;
+					break;
+					case 6: active = false;
+					break;
+					case 7: active = false;
+					break;
+					case 8: active = false;
+					break;
+					default: active = false;
+					break;
+				}
+		return active;}
+		else{return neighborhood[1][1];}
+	}
+}
+
+class Seeds extends Cell{
+	boolean[][] neighborhood = new boolean[3][3];
+	int counter = 0;
+	int cellstate;
+	int a;
+	int b;
+	boolean active;
+	public Seeds(){}
+	public Seeds(int a){ maturity = a;}
+	public boolean iterate(boolean neighborhood[][]){
+		counter += 1;
+		if (counter == maturity){ counter = 0;
+		cellstate = 0;
+		for(b=0;b<=2;b++){
+			for(a=0;a<=2;a++){
+				if (neighborhood[a][b] == true){ cellstate+=1;}
+			}}
+			if(neighborhood[1][1] == true){cellstate-=1;}
+			switch(cellstate){
+					case 0: active = false;
+					break;
+					case 1: active = false;
+					break;
+					case 2: active =  true;
+					break;
+					case 3: active = false;
 					break;
 					case 4: active = false;
 					break;
