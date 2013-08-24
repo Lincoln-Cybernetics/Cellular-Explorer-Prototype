6119f4e56bd40b8af07f287410a7eeb2e3ec5b07
Matt Ahlschwede <perscrumption@hotmail.com>
2013-08-24 02:22:53 -0500

    Create cellComponent.java

---
+++ cellComponent.java
@@ -0,0 +1,116 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+import javax.swing.event.*;
+
+public class cellComponent extends JComponent 
+{
+	int xdim;
+	int ydim;
+	int magnify;
+	int mode = 1;
+	boolean[][] cstate;
+	int[][] species;
+	int[][] lifespan;
+	int[][] orientation;
+	
+	public cellComponent(){
+		xdim = 500;
+		ydim = 300;
+		magnify = 5;
+		cstate = new boolean[xdim][ydim];
+		species = new int[xdim][ydim];
+		lifespan = new int[xdim][ydim];
+		orientation = new int[xdim][ydim];
+		setPreferredSize(new Dimension(xdim*magnify, ydim*magnify));
+	}
+	
+	public cellComponent(int x, int y){
+		xdim = x;
+		ydim = y;
+		magnify = 5;
+		cstate = new boolean[xdim][ydim];
+		species = new int[xdim][ydim];
+		lifespan = new int[xdim][ydim];
+		orientation = new int[xdim][ydim];
+		setPreferredSize(new Dimension(xdim*magnify, ydim*magnify));
+
+	}
+	
+	public void setState(boolean update[][]){
+		cstate = update;
+		repaint();}
+		
+	public void setMode(int a){
+		mode = a; repaint();}
+		
+	public void setMag(int a){
+		magnify = a; repaint();}
+		
+	public void setSpecies(int a, int b, int c){
+		species[a][b] = c;}
+		
+	public void setLifespan(int a, int b, int c){
+		lifespan[a][b] = c;}
+
+
+public void paintComponent( Graphics g){
+					int x = 0;
+					int y = 0;
+					int schmagnify;
+					
+					
+					for(y=0;y<=ydim-1;y++){
+						for(x=0;x<=xdim-1;x++){
+						
+							//normal rendering
+							if (mode == 1){
+								g.setColor(cstate[x][y] ? Color.green : Color.black);
+								g.fillRect(x*magnify,y*magnify,magnify,magnify);}
+						
+							//state editing
+							if (mode == 2){
+								if(magnify>4){schmagnify = magnify-1;}
+								else{schmagnify = magnify;}
+								g.setColor(cstate[x][y] ? Color.green : Color.black);
+								g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify);}
+								
+							// cell editing
+							if(mode == 3){
+								if(magnify>4){schmagnify = magnify-1;}
+								else{schmagnify = magnify;}
+								switch(species[x][y]){
+									case 0: g.setColor(Color.black);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 1: g.setColor(Color.white);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 2: g.setColor(Color.red);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify);break;
+									case 3: g.setColor(Color.blue);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 4: g.setColor(Color.orange);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 5: g.setColor(Color.green);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 6: g.setColor(Color.cyan);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 7: g.setColor(Color.pink);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 8: g.setColor(Color.yellow);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 9: g.setColor(Color.gray);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+									case 10: g.setColor(Color.black);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify);
+											 g.setColor(Color.white);g.fillRect(x*magnify+2,y*magnify+2,schmagnify-2,schmagnify-2); break;
+									case 11: g.setColor(Color.black);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify);
+											 g.setColor(Color.red);g.fillRect(x*magnify+2,y*magnify+2,schmagnify-2,schmagnify-2); break;
+									case 12: g.setColor(Color.black);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify);
+											 g.setColor(Color.blue);g.fillRect(x*magnify+2,y*magnify+2,schmagnify-2,schmagnify-2); break;
+									default: g.setColor(Color.black);g.fillRect(x*magnify,y*magnify,schmagnify,schmagnify); break;
+								}
+								//outline each cell according to its maturity setting
+								switch(lifespan[x][y]){
+									case 1: g.setColor(Color.white); break;
+									case 2: g.setColor(Color.red); break;
+									case 3: g.setColor(Color.blue); break;
+									case 4: g.setColor(Color.black); break;
+									default: g.setColor(Color.green); break;
+								}
+								g.drawRect(x*magnify,y*magnify,magnify,magnify);}
+								
+							
+								
+						}}
+						}
+}
