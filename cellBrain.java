a14a779f488cf826594df6eada4c4af48dffb50a
Matt Ahlschwede <perscrumption@hotmail.com>
2013-08-28 02:56:07 -0500

    Update cellBrain.java

--- cellBrain.java
+++ cellBrain.java
@@ -28,7 +28,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 	int ysiz; // = 300;
 	boolean[][] current; //= new boolean[xsiz][ysiz] ;
 	boolean[][] newstate; //= new boolean[xsiz][ysiz];
-	public Cell[][] culture; //= new Cell[xsiz][ysiz];
+	public cell[][] culture; //= new Cell[xsiz][ysiz];
 	
 	int[][] celltype; //= new int[xsiz][ysiz];
 	int maturity = 1;
@@ -80,7 +80,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 		ysiz = 150;
 		current = new boolean[xsiz][ysiz];
 		newstate = new boolean[xsiz][ysiz];
-		culture = new Cell[xsiz][ysiz];
+		culture = new cell[xsiz][ysiz];
 		celltype = new int[xsiz][ysiz];
 		bigboard = new cellComponent(xsiz, ysiz);
 		bigboard.addMouseMotionListener(this);
@@ -91,7 +91,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 		for(int x=0;x<=xsiz-1;x++){	
 				 current[x][y] = false;
 				 newstate[x][y] = false;
-				 celltype[x][y] = 5;
+				 celltype[x][y] = 6;
 				 maturity = 1;
 				 populate(x,y);
 			
@@ -107,7 +107,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 		ysiz = b;
 		current = new boolean[xsiz][ysiz];
 		newstate = new boolean[xsiz][ysiz];
-		culture = new Cell[xsiz][ysiz];
+		culture = new cell[xsiz][ysiz];
 		celltype = new int[xsiz][ysiz];
 		bigboard = new cellComponent(xsiz, ysiz);
 		bigboard.addMouseMotionListener(this);
@@ -118,7 +118,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 		for(int x=0;x<=xsiz-1;x++){	
 				 current[x][y] = false;
 				 newstate[x][y] = false;
-				 celltype[x][y] = 5;
+				 celltype[x][y] = 6;
 				 maturity = 1;
 				 populate(x,y);
 			
@@ -134,7 +134,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 				garden.getContentPane().add( new JScrollPane(bigboard) );
 				garden.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				garden.setSize(1000,500);
-				garden.setLocation(50,200);
+				garden.setLocation(200,150);
 				garden.setResizable(true);
 				garden.setVisible( true );}
 				
@@ -164,34 +164,47 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 				//makes the cells
 				public void populate(int a, int b){
 					switch (celltype[a][b]){
-						case 0: culture[a][b] = new Cell();
-						break;
-						case 1: culture[a][b] = new onCell();
-						break;
-						case 2: culture[a][b] = new blinkCell(maturity);
-						break;
-						case 3: culture[a][b] = new blinkCell2(maturity);
-						break;
-						case 4: culture[a][b] = new Randcell(maturity);
-						break;
-						case 5: culture[a][b] = new Conway(maturity);
-						break;
-						case 6: culture[a][b] = new Seeds(maturity);
-						break;
-						case 7: culture[a][b] = new OddCell(maturity);
-						break;
-						case 8: culture[a][b] = new EvenCell(maturity);
-						break;
-						case 9: culture[a][b] = new ConveyorCell(maturity, setdir);
-						break;
-						case 10: culture[a][b] = new Wolfram(maturity, setdir);
-						break;
-						case 11: culture[a][b] = new PassiveCell();
-						break;
-						case 12: culture[a][b] = new SymmetriCell(maturity, setdir);
-						break;
-						default: culture[a][b] = new Cell();
-						break;}
+						case 0: culture[a][b] = new cell();
+								break;
+						case 1: culture[a][b] = new offCell();
+								break;
+						case 2: culture[a][b] = new onCell();
+								break;
+						case 3: culture[a][b] = new blinkCell();
+								culture[a][b].setInt("Mat", maturity);
+								break;
+						case 4: culture[a][b] = new seqCell();
+								culture[a][b].setInt("Mat", maturity);
+								break;
+						case 5: culture[a][b] = new randCell();
+								culture[a][b].setInt("Mat", maturity);
+								break;
+						case 6: culture[a][b] = new conway();
+								culture[a][b].setInt("Mat", maturity);
+								break;
+						case 7: culture[a][b] = new seeds();
+								culture[a][b].setInt("Mat", maturity);
+								break;
+						case 8: culture[a][b] = new parityCell();
+								culture[a][b].setInt("Mat", maturity);
+								break;
+						case 9: culture[a][b] = new conveyorCell();
+								culture[a][b].setInt("Mat", maturity);
+								culture[a][b].setInt("Dir", setdir);
+								break;
+						case 10: culture[a][b] = new wolfram();
+								culture[a][b].setInt("Mat", maturity);
+								culture[a][b].setInt("Dir", setdir);
+								break;
+						case 11: culture[a][b] = new symmetriCell();
+								culture[a][b].setInt("Mat", maturity);
+								culture[a][b].setInt("Dir", setdir);
+								break;		
+						case 12: culture[a][b] = new mirrorCell();
+								culture[a][b].setInt("Mat", maturity);
+								break;		
+						default: culture[a][b] = new cell();
+								break;}
 						bigboard.setSpecies(a,b,celltype[a][b]);
 						bigboard.setLifespan(a,b,maturity); 
 					}
@@ -228,7 +241,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 					for(int y=0;y<=ysiz-1;y++){
 					for(int x=0;x<=xsiz-1;x++){
 						cellDraw(x,y);}}
-						bigboard.repaint();
+						if(editcellflag){bigboard.repaint();}
 					}
 					
 					public void cellCheckFill(){
@@ -236,7 +249,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 						for(int x=0;x<=xsiz-1;x++){	
 							cellCheckDraw(x,y);
 							}}
-						bigboard.repaint();
+						if(editcellflag){bigboard.repaint();}
 					}
 					
 					public void cellCheckFilltbt(){
@@ -255,14 +268,14 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 							if(xsiz-1 % 3 != 1){if(ysiz-1 % 2 == 1 ^ xsiz-1 % 2 == 1){
 							tbtPop(xsiz-1,ysiz-1,1);}
 							else{tbtPop(xsiz-1,ysiz-1,2);}  }}
-							bigboard.repaint();}
+							if(editcellflag){bigboard.repaint();}}
 					
 					public void cellRandFill(){
 						for(int y=0;y<=ysiz-1;y++){
 						for(int x=0;x<=xsiz-1;x++){	
 							cellRandDraw(x,y);
 						}}
-						bigboard.repaint();
+						if(editcellflag){bigboard.repaint();}
 					}
 					
 					//sets the cells in a 3x3 area
@@ -365,7 +378,7 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 								case 3: cellCheckDraw(0,y); cellCheckDraw(xsiz-1,y); break;
 								case 4: cellRandDraw(0,y); cellRandDraw(xsiz-1, y); break;
 								default: cellDraw(0,y); cellDraw(xsiz-1, y); break;}}
-								bigboard.repaint();
+								if(editcellflag){bigboard.repaint();}
 					}
 				
 				// State editing methods
@@ -375,19 +388,23 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 					for(int y=0;y<=ysiz-1;y++){
 					for(int x=0;x<=xsiz-1;x++){
 						stateDraw(x,y);}}
-						bigboard.repaint();} 
+					if(bigboard.getMode() == 1 || bigboard.getMode() == 2){bigboard.repaint();}
+					} 
 						
 				public void stateRandFill(){
 					for(int y=0;y<=ysiz-1;y++){
 					for(int x=0;x<=xsiz-1;x++){
 						stateRandDraw(x,y);
-					}} bigboard.repaint();}
+					}} 
+					if(bigboard.getMode() == 1 || bigboard.getMode() == 2){bigboard.repaint();}
+					}
 					
 				public void stateClearFill(){
 					for(int y=0;y<=ysiz-1;y++){
 					for(int x=0;x<=xsiz-1;x++){
 						stateAltDraw(x,y);}}
-						bigboard.repaint();}
+						if(bigboard.getMode() == 1 || bigboard.getMode() == 2){bigboard.repaint();}
+						}
 						
 				public void stateCheckFilltbt(){
 						for(int y=1; y<= ysiz-1; y+=3){
@@ -395,13 +412,16 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 							if(y % 2 == 1 ^ x % 2 == 1){
 							tbtState(x,y,1);}
 							else{tbtState(x,y,2);}}}
-							bigboard.repaint();}
+							if(bigboard.getMode() == 1 || bigboard.getMode() == 2){bigboard.repaint();}
+							}
 						
 				public void stateCheckFill(){
 					for(int y=0;y<=ysiz-1;y++){
 					for(int x=0;x<=xsiz-1;x++){
 					stateCheckDraw(x,y, true);
-					}} bigboard.repaint();}
+					}} 
+					if(bigboard.getMode() == 1 || bigboard.getMode() == 2){bigboard.repaint();}
+					}
 				
 				//state drawing methods	
 				public void stateDraw(int x,int y){
@@ -514,8 +534,14 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 						
 					//neighborhood methods
 					
-						public boolean[][] getMoore(int x, int y){
-						// returns the states of a cell's Moore Neighborhood
+					//returns the "self" neighborhood
+					public boolean[][] getSelf(int x, int y){
+						boolean[][] selfie =new boolean[1][1];
+						selfie[0][0] = current[x][y];
+						return selfie;}
+					
+					// returns the states of a cell's Moore Neighborhood
+					public boolean[][] getMoore(int x, int y){
 					boolean[][] neighbors = new boolean[3][3];
 						//init neighbors WITHOUT resetting x and y
 					neighbors[0][0]=false;neighbors[0][1]=false;neighbors[0][2]=false;
@@ -590,73 +616,73 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 		
 		
 		// neighborhood for one dimensional, horizontal cells
-		public boolean[] getWolfram(int x, int y){
-			boolean[] wolfhood = new boolean[3];
-			if (x == 0){wolfhood[0] = false; if(hwrapflag){wolfhood[0] = current[xsiz-1][y];}} else{wolfhood[0] = current[x-1][y];}
-			wolfhood[1] = current[x][y];
-			if(x == xsiz-1){wolfhood[2] = false;if(hwrapflag){wolfhood[2] = current[0][y];}} else{wolfhood[2] = current[x+1][y];}
+		public boolean[][] getWolfram(int x, int y){
+			boolean[][] wolfhood = new boolean[3][1];
+			if (x == 0){wolfhood[0][0] = false; if(hwrapflag){wolfhood[0][0] = current[xsiz-1][y];}} else{wolfhood[0][0] = current[x-1][y];}
+			wolfhood[1][0] = current[x][y];
+			if(x == xsiz-1){wolfhood[2][0] = false;if(hwrapflag){wolfhood[2][0] = current[0][y];}} else{wolfhood[2][0] = current[x+1][y];}
 			return wolfhood;}
 		
 		// neighborhood for one dimensional, vertical cells	
-		public boolean[] getWolframV(int x, int y){
-			boolean[] wolfhood = new boolean[3];
-			if(y == 0){wolfhood[0] = false;if(vwrapflag){wolfhood[0] = current[x][ysiz-1];}} else{wolfhood[0] = current[x][y-1];}
-			wolfhood[1] = current[x][y];
-			if(y == ysiz-1){wolfhood[2] = false;if(vwrapflag){wolfhood[2] = current[x][0];}} else{wolfhood[2] = current[x][y+1];}
+		public boolean[][] getWolframV(int x, int y){
+			boolean[][] wolfhood = new boolean[3][1];
+			if(y == 0){wolfhood[0][0] = false;if(vwrapflag){wolfhood[0][0] = current[x][ysiz-1];}} else{wolfhood[0][0] = current[x][y-1];}
+			wolfhood[1][0] = current[x][y];
+			if(y == ysiz-1){wolfhood[2][0] = false;if(vwrapflag){wolfhood[2][0] = current[x][0];}} else{wolfhood[2][0] = current[x][y+1];}
 			return wolfhood;}
 			
 		// neighborhood for one dimensional cells starting with upper left
-		public boolean[] getWolframUL(int x, int y){
-			boolean[] wolfhood = new boolean[3];
+		public boolean[][] getWolframUL(int x, int y){
+			boolean[][] wolfhood = new boolean[3][1];
 			if (x == 0 || y == 0){ 
 				//normal border
-				wolfhood[0] = false;
+				wolfhood[0][0] = false;
 				//wraparound border
-				if(hwrapflag){if(x == 0 && y != 0){wolfhood[0] = current[xsiz-1][y-1];}else{wolfhood[0] = false;}}
-			if (vwrapflag){if(y == 0 && x != 0){wolfhood[0] = current[x-1][ysiz-1];} else{wolfhood[0] = false;}}
-			if(vwrapflag && hwrapflag){if(x == 0 && y != 0){wolfhood[0] = current[xsiz-1][y-1];}if(y == 0 && x != 0){wolfhood[0] = current[x-1][ysiz-1];}if(x == 0 && y == 0){wolfhood[0] = current[xsiz-1][ysiz-1];}}}
+				if(hwrapflag){if(x == 0 && y != 0){wolfhood[0][0] = current[xsiz-1][y-1];}else{wolfhood[0][0] = false;}}
+			if (vwrapflag){if(y == 0 && x != 0){wolfhood[0][0] = current[x-1][ysiz-1];} else{wolfhood[0][0] = false;}}
+			if(vwrapflag && hwrapflag){if(x == 0 && y != 0){wolfhood[0][0] = current[xsiz-1][y-1];}if(y == 0 && x != 0){wolfhood[0][0] = current[x-1][ysiz-1];}if(x == 0 && y == 0){wolfhood[0][0] = current[xsiz-1][ysiz-1];}}}
 			//non-border
-			 else{ wolfhood[0] = current[x-1][y-1];}
+			 else{ wolfhood[0][0] = current[x-1][y-1];}
 			 
 			 // center cell
-			wolfhood[1] = current[x][y];
+			wolfhood[1][0] = current[x][y];
 			
 			if (x == xsiz-1 || y == ysiz-1){ 
 				// normal border
-				wolfhood[2] = false;
+				wolfhood[2][0] = false;
 				//wraparound border
-				if(hwrapflag){if (x == xsiz-1 && y != ysiz-1){wolfhood[2] = current[0][y+1];} else {wolfhood[2] = false;}}
-				if(vwrapflag){if(y == ysiz-1 && x != xsiz-1){wolfhood[2] = current[x+1][0];}else{wolfhood[2] = false;}}
-				if(hwrapflag && vwrapflag){if (x == xsiz-1 && y != ysiz-1){wolfhood[2] = current[0][y+1];}if(y == ysiz-1 && x != xsiz-1){wolfhood[2] = current[x+1][0];}if(x == xsiz-1 && y == ysiz-1){wolfhood[2] = current[0][0];}}} 
+				if(hwrapflag){if (x == xsiz-1 && y != ysiz-1){wolfhood[2][0] = current[0][y+1];} else {wolfhood[2][0] = false;}}
+				if(vwrapflag){if(y == ysiz-1 && x != xsiz-1){wolfhood[2][0] = current[x+1][0];}else{wolfhood[2][0] = false;}}
+				if(hwrapflag && vwrapflag){if (x == xsiz-1 && y != ysiz-1){wolfhood[2][0] = current[0][y+1];}if(y == ysiz-1 && x != xsiz-1){wolfhood[2][0] = current[x+1][0];}if(x == xsiz-1 && y == ysiz-1){wolfhood[2][0] = current[0][0];}}} 
 				//non-border
-				else{wolfhood[2] = current[x+1][y+1];}
+				else{wolfhood[2][0] = current[x+1][y+1];}
 			return wolfhood;}
 			
 		// neighborhood for one dimensional cells starting in lower left
-		public boolean[] getWolframLL(int x, int y){
-			boolean[] wolfhood = new boolean[3];
+		public boolean[][] getWolframLL(int x, int y){
+			boolean[][] wolfhood = new boolean[3][1];
 			if(x==0 || y== ysiz-1){
 				// normal border
-				wolfhood[0] = false;
+				wolfhood[0][0] = false;
 				//wraparound borders 
-				if(hwrapflag){if(x == 0 && y != ysiz-1){wolfhood[0] = current[xsiz-1][y+1];} else{wolfhood[0] = false;}}
-				if(vwrapflag){if(y == ysiz-1 && x!= 0){wolfhood[0] = current[x-1][0];}else{wolfhood[0] = false;}}
-				if(hwrapflag && vwrapflag){if(x == 0 && y != ysiz-1){wolfhood[0] = current[xsiz-1][y+1];}if(y == ysiz-1 && x!= 0){wolfhood[0] = current[x-1][0];}if(x == 0 && y == ysiz-1){wolfhood[0] = current[xsiz-1][0];}}}
+				if(hwrapflag){if(x == 0 && y != ysiz-1){wolfhood[0][0] = current[xsiz-1][y+1];} else{wolfhood[0][0] = false;}}
+				if(vwrapflag){if(y == ysiz-1 && x!= 0){wolfhood[0][0] = current[x-1][0];}else{wolfhood[0][0] = false;}}
+				if(hwrapflag && vwrapflag){if(x == 0 && y != ysiz-1){wolfhood[0][0] = current[xsiz-1][y+1];}if(y == ysiz-1 && x!= 0){wolfhood[0][0] = current[x-1][0];}if(x == 0 && y == ysiz-1){wolfhood[0][0] = current[xsiz-1][0];}}}
 			//non-border
-			else{wolfhood[0] = current[x-1][y+1];}
+			else{wolfhood[0][0] = current[x-1][y+1];}
 			
 			// center cell
-			wolfhood[1] = current[x][y];
+			wolfhood[1][0] = current[x][y];
 			
 			if(x == xsiz-1 || y == 0){
 				// normal border
-				wolfhood[2] = false;
+				wolfhood[2][0] = false;
 				//wraparound border
-				if(hwrapflag){if (x == xsiz-1 && y != 0){wolfhood[2] = current[0][y-1];}else{wolfhood[2] = false;}}
-				if(vwrapflag){if(y == 0 && x != xsiz-1){wolfhood[2] = current[x+1][ysiz-1];}else{wolfhood[2] = false;}}
-				if(hwrapflag && vwrapflag){if (x == xsiz-1 && y != 0){wolfhood[2] = current[0][y-1];}if(y == 0 && x != xsiz-1){wolfhood[2] = current[x+1][ysiz-1];}if(x == xsiz-1 && y == 0){wolfhood[2] = current[0][ysiz-1];}}} 
+				if(hwrapflag){if (x == xsiz-1 && y != 0){wolfhood[2][0] = current[0][y-1];}else{wolfhood[2][0] = false;}}
+				if(vwrapflag){if(y == 0 && x != xsiz-1){wolfhood[2][0] = current[x+1][ysiz-1];}else{wolfhood[2][0] = false;}}
+				if(hwrapflag && vwrapflag){if (x == xsiz-1 && y != 0){wolfhood[2][0] = current[0][y-1];}if(y == 0 && x != xsiz-1){wolfhood[2][0] = current[x+1][ysiz-1];}if(x == xsiz-1 && y == 0){wolfhood[2][0] = current[0][ysiz-1];}}} 
 				//non-border
-				else{wolfhood[2] = current[x+1][y-1];}
+				else{wolfhood[2][0] = current[x+1][y-1];}
 			return wolfhood;}
 		
 		//logic methods
@@ -680,22 +706,32 @@ class cellBrain extends JComponent implements Runnable, MouseInputListener
 							newstate[x][y] = culture[x][y].iterate();}
 					
 						if(culture[x][y].getNeighborhood() == "Moore"){
-							newstate[x][y] =culture[x][y].iterate(getMoore(x,y));}
+							culture[x][y].setNeighbors(getMoore(x,y));
+							newstate[x][y] = culture[x][y].iterate();}
 							
 						if(culture[x][y].getNeighborhood() == "Self"){
-							newstate[x][y] = culture[x][y].iterate(current[x][y]);}
+							culture[x][y].setNeighbors(getSelf(x,y));
+							newstate[x][y] = culture[x][y].iterate();}
 							
 						if(culture[x][y].getNeighborhood() == "Wolfram"){
-							newstate[x][y] = culture[x][y].iterate(getWolfram(x,y));}
+							culture[x][y].setNeighbors(getWolfram(x,y));
+							newstate[x][y] = culture[x][y].iterate();}
 							
 						if(culture[x][y].getNeighborhood() == "WolframV"){
-							newstate[x][y] = culture[x][y].iterate(getWolframV(x,y));}
+							culture[x][y].setNeighbors(getWolframV(x,y));
+							newstate[x][y] = culture[x][y].iterate();}
 							
 						if(culture[x][y].getNeighborhood() == "WolframUL"){
-							newstate[x][y] = culture[x][y].iterate(getWolframUL(x,y));}
+							culture[x][y].setNeighbors(getWolframUL(x,y));
+							newstate[x][y] = culture[x][y].iterate();}
 							
 						if(culture[x][y].getNeighborhood() == "WolframLL"){
-							newstate[x][y] = culture[x][y].iterate(getWolframLL(x,y));}
+							culture[x][y].setNeighbors(getWolframLL(x,y));
+							newstate[x][y] = culture[x][y].iterate();}
+							
+						if(culture[x][y].getNeighborhood() == "Mirror"){
+							culture[x][y].setNeighbors(getSelf(culture[x][y].getInt("HX"), culture[x][y].getInt("HY")));
+							newstate[x][y] = culture[x][y].iterate();}
 					}}
 					
 					// cycles new values into current state
