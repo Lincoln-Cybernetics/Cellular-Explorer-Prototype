71d327287be714c3e78944847163db8f5ea9d602
Matt Ahlschwede <perscrumption@hotmail.com>
2013-09-14 02:54:40 -0500

    Update cellOptionHandler.java

--- cellOptionHandler.java
+++ cellOptionHandler.java
@@ -6,6 +6,8 @@ int celltype = 0;
 int maturity = 1;
 int direction = 0;
 boolean inver = false;
+boolean parity = false;
+boolean recursive = false;
 int mirrorx = 0;
 int mirrory = 0;
 public cellOptionHandler(){
@@ -29,6 +31,11 @@ public void setInt(String a, int b){
 	if(a == "MirrY"){ mirrory = b;}
 }
 
+public void setBool(String a, boolean b){
+	if(a == "Par"){parity = b;}
+	if(a == "Rec"){recursive = b;}
+}
+
 //option getting methods
 public int getCT(){ return celltype;}
 
@@ -44,20 +51,36 @@ public int getInt(String a){
 	return 0;
 }
 
+public boolean getBool(String a){
+	if (a == "Par"){return parity;}
+	if (a == "Rec"){return recursive;}
+	return false;
+}
 
 }
 
 class randcellOptionHandler extends cellOptionHandler{
+	int xsiz = 1;
+	int ysiz = 1;
 	Random shovel = new Random();
 	//can not set parameters
 	public void setCT(int a){}
 	public void setMaturity(int a){}
 	public void setDirection(int a){}
 	public void setInvert(boolean a){}
+	public void setInt( String a, int b){
+		if(a == "Xsiz"){xsiz = b;}
+		if(a == "Ysiz"){ysiz = b;}
+	}
+		
 	// generate random cells
 	public int getCT(){ return shovel.nextInt(13);}
 	public int getMaturity(){int ranmat =  shovel.nextInt(4); ranmat +=1; return ranmat;}
 	public int getDirection(){return shovel.nextInt(8);}
 	public boolean getInvert(){return shovel.nextBoolean();}
-	public int getInt(String a){return 0;}
+	public boolean getBool( String a){return shovel.nextBoolean();}
+	public int getInt(String a){
+		if(a == "MirrX"){return shovel.nextInt(xsiz);}
+		if(a == "MirrY"){return shovel.nextInt(ysiz);}
+		return 0;}
 }
