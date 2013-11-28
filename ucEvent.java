4bd594371aa527b0fc7791c2c116af97722df98b
Matt Ahlschwede <perscrumption@hotmail.com>
2013-11-28 03:33:49 -0600

    Create ucEvent.java

---
+++ ucEvent.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*Cellular Explorer Prototype proof of concept
+ * Copyright(C) 02013 Matt Ahlschwede
+ *  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+public class ucEvent extends EventObject{
+	int mycontrol = 0;
+	public ucEvent(Object source){
+		super(source);
+		}
+		
+	public void setCommand(int a){ mycontrol = a;}
+	
+	public int getCommand(){return mycontrol;}
+}
+
+
