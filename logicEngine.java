82b31834f8bcf6615c55d8e6ab7822fe54787f9c
Matt Ahlschwede <perscrumption@hotmail.com>
2013-12-10 01:39:22 -0600

    Create logicEngine.java

---
+++ logicEngine.java
@@ -0,0 +1,13 @@
+
+
+public class logicEngine{
+int rows;
+int cols;
+public cellBrain[][] pistons;
+public cellComponent[][] outputs;
+
+public void logicEngine(){}
+public void iterateInterrupt(int a){}
+public void setMasterSpeed(int a){}
+public int getMasterSpeed(){return 0;}
+}
